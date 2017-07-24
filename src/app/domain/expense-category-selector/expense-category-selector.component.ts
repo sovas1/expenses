4e0b81ed5ad135db6f62a77201bc82443e857ba2
Mariusz Sowa <mariuszsowa11@gmail.com>
2017-07-24 12:25:57 +0200

    init expense-category-selector

---
+++ src/app/domain/expense-category-selector/expense-category-selector.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'expense-category-selector',
+  templateUrl: './expense-category-selector.component.html',
+  styleUrls: ['./expense-category-selector.component.scss']
+})
+
+export class ExpenseCategorySelectorComponent {
+}
